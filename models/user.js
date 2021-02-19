bdb529be7322cd719334aa06b1497f209592752e
Pavlin <forex.xard@gmail.com>
2021-02-19 10:20:37 -0500

    Added logic to user models and created routes for register and login

--- models/user.js
+++ models/user.js
@@ -1,29 +1,80 @@
 /** User class for message.ly */
-
+const db = require("../db");
+const ExpressError = require("../expressError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { BCRYPT_WORK_FACTOR } = require("../config");
 
 
 /** User of the site. */
 
 class User {
+  constructor(username, password, first_name, last_name, phone, join_at, last_login_at){
+    this.username = username;
+    this.password = password;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.phone = phone;
+    this.join_at = join_at;
+    this.last_login_at = last_login_at
+  }
 
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) {
+
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR)
+
+    const result = await db.query(`
+      INSERT INTO users (username, password, first_name, last_name, phone, join_at)
+      VALUES
+      ($1, $2, $3, $4, $5, current_timestamp)
+      RETURNING *
+    `, [username, hashedPassword, first_name, last_name, phone])
+    const newU = result.rows[0]
+    return new User(newU.username, newU.password, newU.first_name, newU.last_name, newU.phone)
+   }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) { 
+    console.log(username, password)
+    const result = await db.query(`
+      SELECT * FROM users WHERE username = $1
+    `, [username])
+
+    const user = result.rows[0]
+
+    if(user){
+      if(await bcrypt.compare(password, user.password)){
+        return true
+      }
+    }else{
+      return false
+    }
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) { 
+    await db.query(`
+      UPDATE users SET last_login_at = current_timestamp
+      WHERE username = $1
+    `, [username])
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const result = await db.query(`
+      SELECT username, first_name, last_name, phone FROM users  
+    `)
+    const users = result.rows.map(u => new User(u.username, u.first_name, u.last_name, u.phone))
+    return users
+   }
 
   /** Get: get user by username
    *
@@ -34,7 +85,15 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) { 
+    const result = await db.query(`
+      SELECT username, first_name, last_name, phone, join_at, last_login_at
+      FROM users 
+      WHERE username = $1
+    `, [username])
+    const user = result.rows[0]
+    return new User(user.username, user.first_name, user.last_name, user.phone, user.join_at, user.last_login_at)
+  }
 
   /** Return messages from this user.
    *
@@ -44,7 +103,13 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {
+    const result = await db.query(`
+      SELECT id, to_username, body, sent_at, read_at
+      FROM messages
+      WHERE to_username = $1
+    `, [username])
+   }
 
   /** Return messages to this user.
    *
@@ -54,7 +119,13 @@ class User {
    *   {id, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) {
+    const result = await db.query(`
+      SELECT id, from_username, body, sent_at, read_at
+      FROM messages
+      WHERE from_username = $1
+    `, [username])
+   }
 }
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,29 @@
+const express = require("express");
+const router = new express.Router();
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config")
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+ 
+ router.post('/login', async (req, res, next) => {
+    try {
+        const {username, password} = req.body;
+        const result = await User.authenticate(username, password)
+        
+        if(result){
+            await User.updateLoginTimestamp(username)
+            const token = jwt.sign({username}, SECRET_KEY)
+            return res.json({msg: 'Loged id', token})
+        }
+    }catch(err) {
+        return next(err);
+    }
+ })
 
 
 /** POST /register - register user: registers, logs in, and returns token.
@@ -11,3 +32,15 @@
  *
  *  Make sure to update their last-login!
  */
+
+ router.post('/register', async (req, res, next) => {
+    
+    try {
+        const result = await User.register(req.body)
+        return res.json(result)
+    }catch (err) {
+        return next(err);
+    }
+ })
+
+module.exports = router;
